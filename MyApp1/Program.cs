c275a9994ff7a6488499dad0eb6ccc812767cab7
aminakhtar <amin.akhtar13@gmail.com>
2022-10-20 14:55:21 -0700

    init

---
+++ MyApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(LengthOfLongestSubstring("au"));
+    }
+    public static int LengthOfLongestSubstring(string s)
+    {
+        int result = 0;
+
+        char[] inputSt = s.ToArray();
+        if(inputSt != null && inputSt.Length > 0)
+        {
+            if (inputSt.Length == 1)
+                return 1;
+
+            int startWindow = 0;
+            int endWindow = 0;
+            string windowSt = inputSt[startWindow].ToString();
+
+            while (endWindow < inputSt.Length - 1)
+            {
+                endWindow++;
+                int indexFoundChar = windowSt.IndexOf(inputSt[endWindow]);
+
+                if (indexFoundChar == -1)
+                { // Not found
+                    windowSt = string.Concat(windowSt, inputSt[endWindow]);
+
+                    if (windowSt.Length > result)
+                        result = windowSt.Length;
+                }
+                else
+                {
+                    if (windowSt.Length > result)
+                        result = windowSt.Length;
+
+                    // Slide the window to start and add one more character
+                    windowSt = windowSt.Substring(++indexFoundChar);
+                    windowSt = string.Concat(windowSt, inputSt[endWindow]);
+                }
+            }
+        }
+
+        return result;
+    }
+}
